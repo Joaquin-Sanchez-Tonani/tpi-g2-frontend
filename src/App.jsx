465a17c9b11e94a7dc29b9e99c89aebac31414d2
Joaquin-Sanchez-Tonani <joaquinsancheztonani15@gmail.com>
2025-08-23 21:25:35 -0300

    Add ScrollToTop when change pathname

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import PageNotFound from './layout/PageNotFound';
 import Layout from './layout/Layout';
+import ScrollToTop from './components/ScrollToTop';
 import { Specialists } from './pages/Specialists';
 
 function App() {
 
   return (
     <BrowserRouter>
+        <ScrollToTop />
         <Routes>
             <Route  path="/" element={<Layout />}>
                 <Route index element={<Home />}/>
